9c8a37d60a5776847f1086fe9d73e92e346e15f0
slothbelphegor <andinhtran123@gmail.com>
2025-09-28 04:21:45 +0000

    fixed cors problem

--- backend/settings.py
+++ backend/settings.py
@@ -166,6 +166,7 @@ USE_TZ = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    'https://lecturer-management-v2-frontend.onrender.com',
 ]
 
 AUTH_USER_MODEL = 'users.CustomUser'
