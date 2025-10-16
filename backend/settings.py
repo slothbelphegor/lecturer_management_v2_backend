fc019ab42e3635caf698838d20237f79933527db
slothbelphegor <andinhtran123@gmail.com>
2025-10-16 14:34:21 +0000

    Changed settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i&h=p212m6957gjnk$v&)$pkont7*%ip9^#1j=w9e*oi8wfcgl'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -100,8 +100,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'tempjapan362@gmail.com'  # Email của bạn
-EMAIL_HOST_PASSWORD = 'wbat xotb lcgv ibvh'  # Mật khẩu email của bạn
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER') 
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
